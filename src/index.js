9a5b5707eaa96dbdad4f223fd0d0c7132f5346a1
Eric Hafvenstein <edhavey@null.net>
2021-10-13 12:43:24 -0500

    10-13-21

--- src/index.js
+++ src/index.js
@@ -1,43 +1,60 @@
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+const IMG_URL = "https://dog.ceo/api/breeds/image/random/4"
+const BREED_URL = 'https://dog.ceo/api/breeds/list/all'
 
-const createBreedLi = (subBreed) => {
-  const dogLi = document.createElement('li');
-  dogLi.textContent = `${ (subBreed)? subBreed : "" } ${breed}`
-  dogBreeds.appendChild(dogLi);
+const breedDropdown = document.querySelector('#breedDropdown');
+const dogBreedsHTML = document.querySelector('#dogBreeds')
+
+// const createBreedLi = (subBreed) => {
+//   const dogLi = document.createElement('li');
+//   dogLi.textContent = `${ (subBreed)? subBreed : "" } ${breed}`
+//   dogBreedsHTML.appendChild(dogLi);
+// }
+
+const pickRdmColor = () => {
+  const rdmVal = () => Math.floor(Math.random() * 255) +1
+  return `rgb(${rdmVal()}, ${rdmVal()}, ${rdmVal()})`
 }
 
-fetch(imgUrl)
+fetch(IMG_URL)
   .then(res => res.json())
   .then(data => {
     const dogImages = data.message;
-    const dogImageContainer = document.querySelector('#dogImageContainer');
+    const dogImageContainerHTML = document.querySelector('#dogImageContainer');
     dogImages.forEach(imgUrl => {
       const dogImg = document.createElement('img');
       dogImg.src = imgUrl;
-      dogImageContainer.appendChild(dogImg);
+      dogImageContainerHTML.appendChild(dogImg);
     })
   })
   .catch(error => console.error(error));
 
-  fetch(breedUrl)
-    .then(res => res.json())
-    .then(data => {
-      const allBreeds = {...data.message};
-      console.dir(allBreeds)
-      const dogBreeds = document.querySelector('#dogBreeds')
-      for (let breed in allBreeds) {
-        let subBreed = "";
-        if (allBreeds[breed].length > 0) {
-          allBreeds[breed].forEach((subBreed) => {
-            const dogLi = document.createElement('li');
-            dogLi.textContent = `${ (subBreed)? subBreed : "" } ${breed}`;
-            dogBreeds.appendChild(dogLi);
-          })
-        } else {
-        const dogLi = document.createElement('li');
-        dogLi.textContent = `${ (subBreed)? subBreed : "" } ${breed}`;
-        dogBreeds.appendChild(dogLi);
-        }
+fetch(BREED_URL)
+  .then(res => res.json())
+  .then(data => {
+    const allBreeds = {...data.message};
+    console.dir(allBreeds)
+    for (let breed in allBreeds) {
+      let subBreed = "";
+      if (allBreeds[breed].length > 0) {
+        allBreeds[breed].forEach((subBreed) => {
+          // this is DRY
+          const dogLi = document.createElement('li');
+          dogLi.textContent = `${ (subBreed)? subBreed : "" } ${breed}`;
+          dogLi.addEventListener('click', e => e.target.style.color = pickRdmColor())
+          dogBreedsHTML.appendChild(dogLi);
+        })
+      } else {
+        // this is DRY
+      const dogLi = document.createElement('li');
+      dogLi.textContent = `${ (subBreed)? subBreed : "" } ${breed}`;
+      
+      dogLi.addEventListener('click', e => e.target.style.color = pickRdmColor())
+      dogBreedsHTML.appendChild(dogLi);
       }
-    })
+    }
+})
+
+breedDropdown.addEventListener('change', e => {
+  const dropdownVal = e.target.value;
+
+})
